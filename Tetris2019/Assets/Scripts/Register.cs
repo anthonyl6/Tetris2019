51a8ddaf9f2940a798dd70a35687fd16811026b3
Anthony Loria <agloria2001@gmail.com>
2019-04-16 21:19:44 -0600

    added an email validation check.
    Added a password length and an email/password check.

--- Tetris2019/Assets/Scripts/Register.cs
+++ Tetris2019/Assets/Scripts/Register.cs
@@ -1,20 +1,66 @@
 ﻿using System;
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Register : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
+    //input field variables
+    [SerializeField] InputField email;
+    [SerializeField] InputField password;
+
+    //check variables
+    bool EC = false;
+    bool PC = false;
+    public void buttonPress()
     {
-        
+        IsValidEmail();
+        passwordCheck();
+        if(EC && PC)
+        {
+            databaseRegister();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    bool IsValidEmail()
     {
+        try 
+        {
+            var check = new System.Net.Mail.MailAddress(email.text);
+            Debug.Log("Email is valid.");
+            EC = true;
+            return check.Address == email.text;
+        }
+        catch 
+        {
+            Debug.Log("Email is not valid.");
+            return false;
+        }
+
+    }
+
+    void passwordCheck()
+    {
+        if(password.text.Length < 8)
+        {
+            Debug.Log("Password must be longer than 8 characters.");
+        }
+        else
+        {
+            PC = true;
+        }
+    }
+
+    void databaseRegister()
+    {
+
         
+
+
+
+
+        Debug.Log("Registered");
     }
 }
